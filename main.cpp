7278c38185ef8a9f5e3681316eac6faaec5db4b2
eduardo segovia <54559794+wairesp@users.noreply.github.com>
2023-06-22 17:57:10 -0500

    Update main.cpp
    Alejar cámara

--- main.cpp
+++ main.cpp
@@ -64,7 +64,7 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
-glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 10.0f);
 glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
 glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
 
